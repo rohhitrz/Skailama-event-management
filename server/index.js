ab19f51b341ca371b52472219a2eacc1f37e15be
Rohit <rohhit.rz@gmail.com>
2025-11-16 01:42:36 +0530

    fixed route for rendering

--- server/index.js
+++ server/index.js
@@ -33,9 +33,9 @@ app.get("/api/health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  // });
 }
 
 const PORT = process.env.PORT || 5014;
