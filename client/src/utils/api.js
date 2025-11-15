8b859a67b6d357ae443d066e7094b68b649a9709
Rohit <rohhit.rz@gmail.com>
2025-11-16 01:55:09 +0530

    deployment fixes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,14 +1,14 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
 
 export const api = {
     createProfile: async (data) => {
-        console.log("initizlizing profile creation", data)
+        // console.log("initizlizing profile creation", data)
         const response = await fetch(`${API_BASE_URL}/profiles`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
         });
-        console.log("checking response", response)
+        // console.log("checking response", response)
 
         if (!response.ok) throw new Error('Failed to create profile');
         return response.json();
